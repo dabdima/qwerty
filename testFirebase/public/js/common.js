f8ced4ea270772f88ee12ab11efade47ded271f6
dabdima <baristin43@gmail.com>
2018-02-25 20:59:42 +0200

    firebase

---
+++ testFirebase/public/js/common.js
@@ -0,0 +1,47 @@
+
+//sidebar
+$("#burger").on("click", function () {
+	if ($(".content aside").attr("style") != "display: none;")
+	$(".content aside, .content aside *").hide("slow");
+	else {
+		$(".content aside *").attr("style", "");
+		$(".content aside").show("slow");
+	}
+	//$(".content aside").toggleClass("aside-hide");
+});
+
+//changing font size for <p> in .middle-content
+$("#fontsize").on("keyup", function () {
+	if (Number($("#fontsize").val()) <= 24 && Number($("#fontsize").val()) >= 8) {
+		$(".right p").attr("style", ("font-size:"+ $("#fontsize").val() + "px; line-height:1.4;"));
+	}
+});
+
+//changing background color
+$("#bg_col").on("keyup", function () {
+	$("#wraper").attr("style", ("background: #"+ $("#bg_col").val() + ";"));
+});
+
+//changing font family
+$("#font_family").on("change", function () {
+	var font_option;
+	switch (Number($("#font_family").val())) {
+	  case 2:
+	    font_option = "serif";
+	    break;
+	  case 3:
+	    font_option = "ubuntu";
+	    break;
+	  case 4:
+	    font_option = "days";
+	    break;
+	  default:
+	    font_option = "sans-serif, Arial";
+	}
+	$("html, body").attr("style", ("font-family: "+ font_option + ";"));
+});
+
+//delete the last p in content
+$("#delete_p").on("click", function () {
+	$('.right p:last').remove();
+});
